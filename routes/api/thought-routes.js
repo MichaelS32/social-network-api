be5b0aa0498bdb451f903dc7e6c3e6b82db7e656
mschneider <michaelschneider3232@gmail.com>
2022-09-04 11:14:46 -0500

    added api user-routes and thought-routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+// Requiring the Express Router
+const router = require('express').Router();
+
+// Set the requirements from thoughts-controller
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addFriend,
+    deleteFriend,
+    addReaction
+} = require('../../controllers/thoughts-controllers');
+
+// /api/thoughts <GET>
+router.route('/').get(getAllThoughts)
+
+// /api/users/:id <GET, PUT, DELETE>
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts);
+
+// /api/thoughts/:userId <POST>
+router.route('/:userId').post(createThoughts);
+
+// /api/thoughts/:thoughtId/reactions <POST>
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactionId <DELETE>
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaciton);
+
+// exporting router
+module.exports = router;
